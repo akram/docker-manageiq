4980621c723c8525452d8a38d3a9322136056c96
Akram Ben Aissi <akram@redhat.com>
2016-03-15 10:44:51 +0100

    Adding launch script

---
+++ launchManageIQ.sh
@@ -0,0 +1,17 @@
+echo "waiting for the DB to start"
+sleep 5
+if [ -e /var/lib/pgsql/initialized ]
+then
+	echo "Reusing existing DB"
+	cd /manageiq/vmdb
+else
+	echo "Init DB"
+	sudo -u postgres sh /createDB.sh
+	cd /manageiq/vmdb
+	bin/rake db:migrate
+	touch /var/lib/pgsql/initialized
+fi
+bin/rake evm:start
+tail -f log/evm.log
+
+
