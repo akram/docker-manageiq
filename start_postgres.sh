e00290c1c7ba5dd45b31cb80b226642b1a5f5ce8
Akram Ben Aissi <akram@redhat.com>
2016-03-15 10:12:19 +0100

    Launching in background

--- start_postgres.sh
+++ start_postgres.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
+echo "Starting progress daemon"
 # Start postgres daemon
-exec su postgres -c "/usr/pgsql-9.4/bin/postgres -D /var/lib/pgsql/9.4/data"
+exec su postgres -c "/usr/pgsql-9.4/bin/postgres -D /var/lib/pgsql/9.4/data" &
+echo "Starting progress daemon: Done"
 
+echo "Updating postgres default password"
 # Update default postgres user password
-exec su postgres -c "psql -c \"alter user postgres password 'password'\";"
+exec su postgres -c "psql -c \"alter user postgres password 'password'\";" &
+echo "Updating postgres default password: Done"
+
